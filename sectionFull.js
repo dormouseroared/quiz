83890d018694250d1b2b8aa0fa304f3e18e60323
dormouseroared <dormouseroared@protonmail.com>
2025-05-20 20:47:25 +0100

    Full or Intermediate section can be imported

---
+++ sectionFull.js
@@ -0,0 +1,14 @@
+const section = [
+    { id: 0, name: "dummy", questions: 0, notes: "58 questions in 2 hours with pass at 35 correct or 60%" },
+    { id: 1, name: "Licensing conditions", questions: 7, weeks: [] },
+    { id: 2, name: "Technical aspects", questions: 11, weeks: [] },
+    { id: 3, name: "Transmitters and receivers", questions: 12, weeks: [] },
+    { id: 4, name: "Feeders and antennas", questions: 4, weeks: [] },
+    { id: 5, name: "Propagation", questions: 3, weeks: [] },
+    { id: 6, name: "Electro magnetic compatibility", questions: 10, weeks: [] },
+    { id: 7, name: "Operating practices and procedures", questions: 2, weeks: [] },
+    { id: 8, name: "Safety", questions: 4, weeks: [] },
+    { id: 9, name: "Measurements and construction", questions: 5, weeks: [] },
+]
+
+export default section
---
+++ sectionIntermediate.js
@@ -0,0 +1,14 @@
+const section = [
+    { id: 0, name: "dummy", questions: 0 },
+    { id: 1, name: "Licensing conditions", questions: 6, weeks: [11] },
+    { id: 2, name: "Technical aspects", questions: 14, weeks: [2, 3, 4, 5, 6, 7] },
+    { id: 3, name: "Transmitters and receivers", questions: 7, weeks: [8, 9, 10] },
+    { id: 4, name: "Feeders and antennas", questions: 4, weeks: [12, 13] },
+    { id: 5, name: "Propagation", questions: 3, weeks: [11] },
+    { id: 6, name: "Electro magnetic compatibility", questions: 4, weeks: [13] },
+    { id: 7, name: "Operating practices and procedures", questions: 2, weeks: [1] },
+    { id: 8, name: "Safety", questions: 3, weeks: [1] },
+    { id: 9, name: "Measurements and construction", questions: 3, weeks: [1] },
+]
+
+export default section
