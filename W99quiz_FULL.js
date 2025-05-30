917cb91a464c0fed3d8ca95a0a08836bc4ca59ba
dormouseroared <dormouseroared@protonmail.com>
2025-05-30 19:58:06 +0100

    exam secrets for syllabus 1 complete, italic for excl btn if blank

--- W99quiz_FULL.js
+++ W99quiz_FULL.js
@@ -1,3 +1,4 @@
+import EXAM_SECRETS_SAMPLES from "./EXAM_SECRETS_SAMPLES.js"
 import RSGB_FULL_MOCK_1 from "./RSGB_FULL_MOCK_1.js"
 import RSGB_FULL_MOCK_2 from "./RSGB_FULL_MOCK_2.js"
 import RSGB_FULL_MOCK_3 from "./RSGB_FULL_MOCK_3.js"
@@ -5,7 +6,8 @@ import RSGB_FULL_MOCK_3 from "./RSGB_FULL_MOCK_3.js"
 const W99quiz = [
     ...RSGB_FULL_MOCK_1,
     ...RSGB_FULL_MOCK_2,
-    ...RSGB_FULL_MOCK_3
+    ...RSGB_FULL_MOCK_3,
+    ...EXAM_SECRETS_SAMPLES
 ]
 
 export default W99quiz
