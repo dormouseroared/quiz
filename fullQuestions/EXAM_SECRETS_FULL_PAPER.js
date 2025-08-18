f67e34260ec2862536562f52af6b82df310ff2b3
dormouseroared <dormouseroared@protonmail.com>
2025-08-18 23:03:07 +0100

    next batch of Full Paper in Secrets

---
+++ fullQuestions/EXAM_SECRETS_FULL_PAPER.js
@@ -0,0 +1,139 @@
+const EXAM_SECRETS_FULL_PAPER = [
+    {
+        source: "Exam Secrets FULL PAPER Q01",
+        question: "Which action is prohibited by the licence, unless carried out in support of a user service, disaster or international emergency?",
+        image: "",
+        options: [
+            "sending messages in a code such as Morse Code, which is not widely understood",
+            "using abbreviations which are not listed in the relevant examination syllabus",
+            "using words where the meaning is known only to the intended recipient",
+            "speaking in a foreign language not commonly spoken in the UK",
+        ],
+        correct: 2,
+        explanation: "(6-19b) prohibits obscuring the meaning of messages unless for the purposes of (6-5) which is supporting user services or disasters. Morse and Q-codes are to enhance communication, and are fine, as is using any language both parties happen to share.",
+        lookup: "See page(s) x of the Full Licence text book",
+        syllabus: "0"
+    },
+    {
+        source: "Exam Secrets FULL PAPER Q02",
+        question: "Why is it appropriate to take steps to control the transmitted bandwidth?",
+        image: "",
+        options: [
+            "the transmission will occupy more spectrum than required for satisfactory communication and may contravene licence conddistions 6-2",
+            "spread spectrum moddes are not permitted by the amateur radio licence, unless a special research permit is obtained from Ofcom",
+            "the signal will be unintelligible on most receivers",
+            "the transmitted frequency will be unstable",
+        ],
+        correct: 0,
+        explanation: "(6-2) requires us not to cause undue interference to any wireless telegraphy, including other amateurs. A transmission wider than that necessary for successful communication risks doing that and is, in any case, anti-social.",
+        lookup: "See page(s) x of the Full Licence text book",
+        syllabus: "0"
+    },
+    {
+        source: "Exam Secrets FULL PAPER Q03",
+        question: "A remotely controlled transmitter is on 7.11MHz, and the control link is on 145.795MHz. The main transmitter is set to a power level of 80W ERP. The maximum permitted power limit for the link is:",
+        image: "",
+        options: [
+            "10W",
+            "40W ERP",
+            "80W ERP",
+            "1000W",
+        ],
+        correct: 3,
+        explanation: "The only technical requirement for a link in an amateur bandd is that it must be above 30MHz, so the licence limit is 1kW at each end. Obviously, and to comply with (6-2) on undue interference, the link should be at the lowest power that allows reliable operation.",
+        lookup: "See page(s) x of the Full Licence text book",
+        syllabus: "0"
+    },
+    {
+        source: "Exam Secrets FULL PAPER Q04",
+        question: "If you have your HAREC document with you, when in a country that has signed up to CEPT recommendation T/R 61-02, then you may:",
+        image: "",
+        options: [
+            "operate in that country for longer than the normal 3 month limit",
+            "apply for an amateur licence in that country as if you had passed their examination",
+            "operate on bands that are in either that countries licence or you UK licence",
+            "drop the normal requirement to prefix your UK callsign with the local prefix letters",
+        ],
+        correct: 1,
+        explanation: "The Harmonised Amateur Radio Examination Certificate (HAREC) confirms you have passed the Full level examination, or its equivalent in signatory countries, and can obtain a licence from that country.",
+        lookup: "See page(s) x of the Full Licence text book",
+        syllabus: "0"
+    },
+    {
+        source: "Exam Secrets FULL PAPER Q05",
+        question: "The UK is in ITU Region:",
+        image: "",
+        options: [
+            "1",
+            "2",
+            "3",
+            "4",
+        ],
+        correct: 0,
+        explanation: "The UK and Europe are in Region 1.",
+        lookup: "See page(s) x of the Full Licence text book",
+        syllabus: "0"
+    },
+    {
+        source: "Exam Secrets FULL PAPER Q06",
+        question: "",
+        image: "",
+        options: [
+            "",
+            "",
+            "",
+            "",
+        ],
+        correct: 9,
+        explanation: "",
+        lookup: "See page(s) x of the Full Licence text book",
+        syllabus: "0"
+    },
+    {
+        source: "Exam Secrets FULL PAPER Q07",
+        question: "",
+        image: "",
+        options: [
+            "",
+            "",
+            "",
+            "",
+        ],
+        correct: 9,
+        explanation: "",
+        lookup: "See page(s) x of the Full Licence text book",
+        syllabus: "0"
+    },
+    {
+        source: "Exam Secrets FULL PAPER Q08",
+        question: "",
+        image: "",
+        options: [
+            "",
+            "",
+            "",
+            "",
+        ],
+        correct: 9,
+        explanation: "",
+        lookup: "See page(s) x of the Full Licence text book",
+        syllabus: "0"
+    },
+    {
+        source: "Exam Secrets FULL PAPER Q00",
+        question: "",
+        image: "",
+        options: [
+            "",
+            "",
+            "",
+            "",
+        ],
+        correct: 9,
+        explanation: "",
+        lookup: "See page(s) x of the Full Licence text book",
+        syllabus: "0"
+    },
+]
+
+export default EXAM_SECRETS_FULL_PAPER
