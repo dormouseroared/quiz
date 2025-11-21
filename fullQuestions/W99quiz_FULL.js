df44e96d24e871c1f5667b5d8927ad1616413db6
dormouseroared <dormouseroared@protonmail.com>
2025-11-21 00:10:20 +0000

    Finally removinbg Claude Quiz from question bank

--- fullQuestions/W99quiz_FULL.js
+++ fullQuestions/W99quiz_FULL.js
@@ -5,17 +5,17 @@ import RSGB_FULL_MOCK_2 from "./RSGB_FULL_MOCK_2.js"
 import RSGB_FULL_MOCK_3 from "./RSGB_FULL_MOCK_3.js"
 import BBDL_FULL_QUIZZES from "./BBDL_FULL_QUIZZES.js"
 import OWN_QUESTIONS from "./OWN_QUESTIONS.js"
-import CLAUDE_QUIZ from "./CLAUDE_QUIZ.js"
+// import CLAUDE_QUIZ from "./CLAUDE_QUIZ.js"
 
 const W99quiz = [
-    ...RSGB_FULL_MOCK_1,
-    ...RSGB_FULL_MOCK_2,
-    ...RSGB_FULL_MOCK_3,
-    ...EXAM_SECRETS_QUESTIONS,
-    ...EXAM_SECRETS_FULL_PAPER,
-    ...BBDL_FULL_QUIZZES,
-    ...OWN_QUESTIONS,
-    ...CLAUDE_QUIZ
+  ...RSGB_FULL_MOCK_1,
+  ...RSGB_FULL_MOCK_2,
+  ...RSGB_FULL_MOCK_3,
+  ...EXAM_SECRETS_QUESTIONS,
+  ...EXAM_SECRETS_FULL_PAPER,
+  ...BBDL_FULL_QUIZZES,
+  ...OWN_QUESTIONS,
+  // ...CLAUDE_QUIZ
 ]
 
 export default W99quiz
